34a27775d0d260808fb7787a0d0ad32312b18657
mnik01 <maxim.nikon78@gmail.com>
2020-04-12 21:04:56 +0500

    Updates

---
+++ precache-manifest.a9b76da54ec77ea77de61dc8a0990ded.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8032fc12e9160873172e5bc3c6c6fc53",
+    "url": "/player/index.html"
+  },
+  {
+    "revision": "01f66d2205940cb9e343",
+    "url": "/player/static/css/main.b118c38f.chunk.css"
+  },
+  {
+    "revision": "16aab28f2ef4b87292f5",
+    "url": "/player/static/js/2.bce1cd79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/player/static/js/2.bce1cd79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f66d2205940cb9e343",
+    "url": "/player/static/js/main.11b765a9.chunk.js"
+  },
+  {
+    "revision": "e362d7ee8312b55dfdec",
+    "url": "/player/static/js/runtime-main.a97dc129.js"
+  },
+  {
+    "revision": "60c83159aba4a22bc9cbbe623d39232a",
+    "url": "/player/static/media/Brianstorm.60c83159.mp3"
+  },
+  {
+    "revision": "78281cd8466d2dc92cd6f64c3f196c13",
+    "url": "/player/static/media/TeddyPicker.78281cd8.mp3"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/player/static/media/git.1bb02710.svg"
+  },
+  {
+    "revision": "d8914c90f7b20740eb449b8a9babb569",
+    "url": "/player/static/media/next.d8914c90.svg"
+  },
+  {
+    "revision": "80e250eb84473dfeb1e057b1d86f09e4",
+    "url": "/player/static/media/pause.80e250eb.svg"
+  },
+  {
+    "revision": "061d845f78e3b0d340f6f7b69a4328a0",
+    "url": "/player/static/media/play.061d845f.svg"
+  },
+  {
+    "revision": "2cbac354d80ae148204e96b396eb12c4",
+    "url": "/player/static/media/prev.2cbac354.svg"
+  },
+  {
+    "revision": "673d6e75f17ef5cb3f5840600d91f1c4",
+    "url": "/player/static/media/thumb.673d6e75.jpg"
+  }
+]);
